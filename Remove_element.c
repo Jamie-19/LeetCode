d4ef7432ced5e4fc55ea4426916a1ced134ca7c4
Jamie-19 <jamesjoseph190403@gmail.com>
2023-10-04 23:16:17 +0530

    answer in C to the 27th problem of leetcode

---
+++ Remove_element.c
@@ -0,0 +1,43 @@
+/*
+27.Remove an element
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.    
+*/
+
+#include<stdio.h>   
+int removeElement(int* nums, int numsSize, int val);
+int main()
+{
+    int nums[100],numsSize,val,i;
+    printf("Enter the size of array: asdadadasd");
+    scanf("%d",&numsSize);
+    printf("Enter the elements of array: ");
+    for(i=0;i<numsSize;i++)
+    {
+        scanf("%d",&nums[i]);
+    }
+    printf("Enter the value to be removed: ");
+    scanf("%d",&val);
+    int k=removeElement(nums,numsSize,val);
+    printf("The number of elements in nums which are not equal to val are: %d",k);
+    for(i=0;i<k;i++)
+    {
+        printf("\n%d \t",nums[i]);
+    }
+    return 0;
+}
+
+int removeElement(int* nums, int numsSize, int val)
+{
+    {
+        int k=0,i;
+        for(i=0;i<numsSize;i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[k]=nums[i];
+                k+=1;
+            }
+        }
+        return k;
+    }
+}
