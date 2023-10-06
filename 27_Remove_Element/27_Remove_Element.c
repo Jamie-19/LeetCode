991281595b58dcd07cc04eef3b69fc2149bc4c08
Jamie-19 <jamesjoseph190403@gmail.com>
2023-10-07 00:14:51 +0530

    .

--- 27_Remove_Element/27_Remove_Element.c
+++ 27_Remove_Element/27_Remove_Element.c
@@ -1,6 +1,7 @@
 /*
 27.Remove an element
-Given an array nums and a value val, remove all instances of that value in-place and return the new length.    
+Given an array nums and a value val, 
+remove all instances of that value in-place and return the new length.    
 */
 
 #include<stdio.h>   
