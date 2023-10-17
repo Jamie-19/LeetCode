7edfced0e7234f4403385785d515d466edcd5e2b
Jamie-19 <mdl21cs057@mec.ac.in>
2023-10-18 01:37:07 +0530

    greedy solution

--- 45_Jump_Game_II/45_Jump_Game_II.c
+++ 45_Jump_Game_II/45_Jump_Game_II.c
@@ -13,12 +13,12 @@ Return the minimum number of jumps to reach nums[n - 1].
 
 #include<stdio.h>
 int jump(int* nums, int numsSize);
-int main()
+int main()  //main function
 {
     int nums[100],numsSize,i;
-    printf("Enter the size of array: ");
+    printf("Enter the size of array: "); //taking input from user
     scanf("%d",&numsSize);
-    printf("Enter the elements of array: ");
+    printf("Enter the elements of array: "); //taking the elements of array from user
     for(i=0;i<numsSize;i++)
     {
         scanf("%d",&nums[i]);
@@ -29,18 +29,31 @@ int main()
 }
 
 int jump(int* nums, int numsSize){
-    int l=0,r=0,count=0;
-    while(r<numsSize-1){
-        int max=0;
-        for(int i=l;i<=r;i++){
-            if(i+nums[i]>max){
-                max=i+nums[i];
-            }
-        }
-        l=r+1;
-        r=max;
-        count++;
-    }
-    return count;
+    int count=0; //for counting the number of jumps
 
+    int max=0; //for storing the maximum jump
+ 
+    int lastjump=0; //for storing the last jump
+ 
+    int dest=numsSize-1; //for storing the destination
+
+    if(numsSize==1) return 0; //if there is only one element in the array then return 0
+    
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]+i>max){
+            max=nums[i]+i; //storing the maximum jump
+        }
+      
+        if(i==lastjump){ //if the index is equal to the last jump then increment the count and update the last jump
+            count++;
+            lastjump=max;
+        }
+       
+        if(lastjump>=dest){ //if the last jump is greater than or equal to the destination then return the count
+            return count;
+        }
+    }
+    return count; 
 }
+//Time Complexity: O(n)
+//Space Complexity: O(1)
