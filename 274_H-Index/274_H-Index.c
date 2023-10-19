080d9853af38e6b903c39a590f60b54641cd0b04
Jamie-19 <mdl21cs057@mec.ac.in>
2023-10-20 02:03:31 +0530

    using array rotation

--- 274_H-Index/274_H-Index.c
+++ 274_H-Index/274_H-Index.c
@@ -5,47 +5,47 @@ return the researcher's h-index.
 The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
 */
 
-
 #include<stdio.h>
-int hIndex(int* citations, int citationsSize);
+void rotate(int* nums, int numsSize, int k);
 int main()
 {
-    int citations[100],citationsSize,i;
-    // taking user input
-    printf("Enter the size of array: "); 
-    scanf("%d",&citationsSize);
+    int nums[100],numsSize,i,k;
+    //int nums[]={3,2,2,3};
+    //taking input from user
+    printf("Enter the size of array: ");
+    scanf("%d",&numsSize);
     printf("Enter the elements of array: ");
-    for(i=0;i<citationsSize;i++)
+    for(i=0;i<numsSize;i++)
     {
-        scanf("%d",&citations[i]);
+        scanf("%d",&nums[i]);
     }
-    printf("The h-index is: %d",hIndex(citations,citationsSize));
+    printf("Enter the value of k: ");
+    scanf("%d",&k);
+    rotate(nums,numsSize,k);
+    printf("The rotated array is: ");
+    for(i=0;i<numsSize;i++)
+    {
+        printf("%d ",nums[i]);
+    }
     return 0;
 }
 
-int hIndex(int* citations, int citationsSize)
+void rotate(int* nums, int numsSize, int k)
 {
     int i,j,temp;
-    // sorting the array in ascending order
-    for(i=0;i<citationsSize-1;i++)
+    for(i=0;i<k;i++)
     {
-        for(j=0;j<citationsSize-i-1;j++)
+        temp=nums[numsSize-1];
+        for(j=numsSize-1;j>0;j--)
         {
-            if(citations[j]>citations[j+1])
-            {
-                temp=citations[j];
-                citations[j]=citations[j+1];
-                citations[j+1]=temp;
-            }
+            nums[j]=nums[j-1];
         }
+        nums[0]=temp;
     }
-    // finding the h-index
-    for(i=1;i<=citationsSize;++i)
-    {
-        if(citations[citationsSize-i] < i) // checks if the number of citations is less than the h-index.if less then return the previous value of i
-        {
-            return i-1;
-        }
-    }
-    return citationsSize;
 }
+
+// Time Complexity: O(n*k)
+// Space Complexity: O(1)
+
+
+
